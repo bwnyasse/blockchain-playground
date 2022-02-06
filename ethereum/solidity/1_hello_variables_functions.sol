72143120a3b9df22b73bdb6ede19829f25c14894
bwnyasse <risbonyasse@gmail.com>
2022-02-06 01:38:16 -0500

    eth(sol): fixed size arrays

--- ethereum/solidity/1_hello_variables_functions.sol
+++ ethereum/solidity/1_hello_variables_functions.sol
@@ -1,42 +1,41 @@
 //SPDX-License-Identifier: GPL-3.0
- 
+
 pragma solidity >=0.5.0 <0.9.0;
- 
-contract Property{
+
+contract Property {
     // declaring state variables saved in contract's storage
-    uint price; // by default is private
+    uint256 price; // by default is private
     string public location;
-    
+
     // can be initialized at declaration or in the constructor only
-    address immutable public owner; 
-    
+    address public immutable owner;
+
     // declaring a constant
-    int constant area = 100;
-    
+    int256 constant area = 100;
+
     // declaring the constructor
     // is executed only once at contract's deployment
-    constructor(uint _price, string memory _location){
+    constructor(uint256 _price, string memory _location) {
         price = _price;
         location = _location;
-        owner = msg.sender;  // initializing owner to the account's address that deploys the contract
+        owner = msg.sender; // initializing owner to the account's address that deploys the contract
     }
-    
-    
+
     // getter function, returns a state variable
-    // a function declared `view` does not alter the blockchain 
-    function getPrice() public view returns(uint){
+    // a function declared `view` does not alter the blockchain
+    function getPrice() public view returns (uint256) {
         return price;
     }
-    
+
     // setter function, sets a state variable
-    function setPrice(uint _price) public{
-        int a; // local variable saved on stack
+    function setPrice(uint256 _price) public {
+        int256 a; // local variable saved on stack
         a = 10;
         price = _price;
     }
-    
-    function setLocation(string memory _location) public{ //string types must be declared memory or storage
+
+    function setLocation(string memory _location) public {
+        //string types must be declared memory or storage
         location = _location;
     }
-    
 }
