97c0b85871ffcdd97461217e8fb7b41ecdf67f5c
bwnyasse <risbonyasse@gmail.com>
2022-02-10 23:05:50 -0500

    eth(project): lottery challenges

---
+++ ethereum/projects/lottery/challenges/lottery_challenge_2_ok.sol
@@ -0,0 +1,72 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+/*
+Change the contract so that the manager is automatically 
+added to the lottery, without sending any ether.
+*/
+contract Lottery {
+    // declaring the state variables
+    address payable[] public players; //dynamic array of type address payable
+    address public manager;
+
+    // declaring the constructor
+    constructor() {
+        // initializing the owner to the address that deploys the contract
+        manager = msg.sender;
+
+        // automatically add manager into the lottery without sending ether
+        players.push(payable(manager));
+    }
+
+    // declaring the receive() function that is necessary to receive ETH
+    receive() external payable {
+        // each player sends exactly 0.1 ETH
+        require(msg.value == 0.1 ether);
+        // appending the player to the players array
+        players.push(payable(msg.sender));
+    }
+
+    // returning the contract's balance in wei
+    function getBalance() public view returns (uint256) {
+        // only the manager is allowed to call it
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+
+    // helper function that returns a big random integer
+    function random() internal view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        players.length
+                    )
+                )
+            );
+    }
+
+    // selecting the winner
+    function pickWinner() public {
+        // only the manager can pick a winner if there are at least 3 players in the lottery
+        require(msg.sender == manager);
+        require(players.length >= 3);
+
+        uint256 r = random();
+        address payable winner;
+
+        // computing a random index of the array
+        uint256 index = r % players.length;
+
+        winner = players[index]; // this is the winner
+
+        // transferring the entire contract's balance to the winner
+        winner.transfer(getBalance());
+
+        // resetting the lottery for the next round
+        players = new address payable[](0);
+    }
+}
